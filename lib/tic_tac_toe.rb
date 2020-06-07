0d41d30403cc3b998cb7373176fbf2a7dd619e6b
Serhiy Bardysh <sbardysh@tutanota.com>
2020-06-07 02:12:53 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -26,11 +26,9 @@ puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
 @display_board
   end
 
-
   def input_to_index(user_input)
     user_input.to_i - 1
   end
-
 
   def position_taken?(index)
     !(@board[index].nil? || board[index] == " ")
