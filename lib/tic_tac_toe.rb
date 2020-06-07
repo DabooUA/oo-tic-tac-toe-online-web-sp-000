288081bb207b7282822dd59f6842af8e2d642915
Serhiy Bardysh <sbardysh@tutanota.com>
2020-06-07 02:09:11 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -67,7 +67,7 @@ puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
     p_token = current_player
   if valid_move?(board, index)
     move(board, index, p_token)
-    display_board
+    @display_board
   else
     turn(board)
     end
