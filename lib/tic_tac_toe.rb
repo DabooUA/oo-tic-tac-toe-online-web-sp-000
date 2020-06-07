f3e43d96cc9d2ba798a6bd4b2b1afab5809f1873
Serhiy Bardysh <sbardysh@tutanota.com>
2020-06-07 02:11:08 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -37,7 +37,7 @@ puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
 
 
-  def move(board, index, current_player)
+  def move(index, current_player)
     board[index] = current_player
   end
 
